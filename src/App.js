30127e82d118329c2facba27003d0818baa27f0f
Zeev <zeevgoshen@gmail.com>
2022-03-17 22:36:18 +0200

    added navlinks in header

--- src/App.js
+++ src/App.js
@@ -9,19 +9,23 @@ import {
 
 import Users from "./user/pages/Users";
 import NewPlace from "./user/pages/NewPlace";
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
 
 const App = () => {
   return (
     <Router>
-      <Switch>
-        <Route path="/" exact>
-          <Users />
-        </Route>
-        <Route path="/places/new" exact>
-          <NewPlace />
-        </Route>
-        <Redirect to="/" />
-      </Switch>
+      <MainNavigation />
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <Users />
+          </Route>
+          <Route path="/places/new" exact>
+            <NewPlace />
+          </Route>
+          <Redirect to="/" />
+        </Switch>
+      </main>
     </Router>
   );
 };
