74acb654f117f2de92160d174a1c017f79ead4d6
Zeev <zeevgoshen@gmail.com>
2022-03-17 23:27:48 +0200

    mobile drawer initial

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -10,24 +10,6 @@ const Users = () => {
       image:"https://images.pexels.com/photos/344060/pexels-photo-344060.jpeg?cs=srgb&dl=pexels-alexandr-podvalny-344060.jpg&fm=jpg",
       places: 3
     },
-    {
-        id: "u2",
-        name: "zeev goshen",
-        image:"https://images.pexels.com/photos/344060/pexels-photo-344060.jpeg?cs=srgb&dl=pexels-alexandr-podvalny-344060.jpg&fm=jpg",
-        places: 3
-      },
-      {
-        id: "u3",
-        name: "zeev goshen",
-        image:"https://images.pexels.com/photos/344060/pexels-photo-344060.jpeg?cs=srgb&dl=pexels-alexandr-podvalny-344060.jpg&fm=jpg",
-        places: 3
-      },
-      {
-        id: "u4",
-        name: "zeev goshen",
-        image:"https://images.pexels.com/photos/344060/pexels-photo-344060.jpeg?cs=srgb&dl=pexels-alexandr-podvalny-344060.jpg&fm=jpg",
-        places: 3
-      },
   ];
   return <UsersList items={USERS} />;
 };
