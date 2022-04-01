d197cfc63e4f50188364e2c86c16411ac3815bd3
Ze'ev Goshen <zeevgoshen@gmail.com>
2022-04-02 00:21:03 +0300

    maps working

--- src/shared/components/UIElements/Map.js
+++ src/shared/components/UIElements/Map.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 
-import './Map.css';
+import "./Map.css";
 
-const Map = props => {
-    return <div className={`map ${props.className}`} style={props.style}></div>
+const Map = (props) => {
+  const mapRef = useRef();
+
+  const { center, zoom } = props; // default javascript feature - object destructuring, pulls elements out props;
+  // pulls keys and their values out of props, and stores them in new constants
+
+  useEffect(() => {
+    const map = new window.google.maps.Map(mapRef.current, {
+      center: center,
+      zoom: zoom,
+    });
+
+    new window.google.maps.Marker({ position: center, map: map });
+  }, [center, zoom]);
+
+  return (
+    <div
+      ref={mapRef}
+      className={`map ${props.className}`}
+      style={props.style}
+    ></div>
+  );
 };
 
 export default Map;
