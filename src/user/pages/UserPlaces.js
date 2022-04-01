032a020f4e061114efa2ecb337d1a60abed6da73
Ze'ev Goshen <zeevgoshen@gmail.com>
2022-04-01 21:38:04 +0300

    Adding custom buttons

--- src/user/pages/UserPlaces.js
+++ src/user/pages/UserPlaces.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+import { useParams } from 'react-router-dom';
 import PlaceList from "../../places/components/PlaceList";
 
 const DUMMY_PLACES = [
@@ -30,7 +31,9 @@ const DUMMY_PLACES = [
 ];
 
 const UserPlaces = () => {
-    return <PlaceList items={DUMMY_PLACES} />;
+    const userId = useParams().userId;
+    const loadedPlaces = DUMMY_PLACES.filter(place => place.creator === userId);
+    return <PlaceList items={loadedPlaces} />;
 };
 
 export default UserPlaces;
