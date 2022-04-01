ec4ab363278b27c23810a5c194f0112dbebb8f8c
Ze'ev Goshen <zeevgoshen@gmail.com>
2022-04-01 23:50:15 +0300

    Maps api

---
+++ src/shared/components/UIElements/Map.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import './Map.css';
+
+const Map = props => {
+    return <div className={`map ${props.className}`} style={props.style}></div>
+};
+
+export default Map;
